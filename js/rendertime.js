aa20a87551bcc26ce584e40defe29c11b66b41ed
Vinc3r <Vinc3r@users.noreply.github.com>
2017-10-12 14:06:26 +0200

    trying to watch user input
    bug when manual setting value

--- js/rendertime.js
+++ js/rendertime.js
@@ -1,18 +1,39 @@
 function on_load(){
-    
-    /* init time select elements (minutes and seconds) */
-    var selectHTMLArray = [];
+
+/* init all select elements */
+
+    /* time select element, limited to [0,59] */
+    var sexagesimalArray = [];
     for(var i = 0; i < 60; i++){
-        selectHTMLArray.push(i);
+        sexagesimalArray.push(i);
     };
-    var selectHTMLselected = 0;
-    set_html_select_time("animDtnM", selectHTMLArray, selectHTMLselected);
-    set_html_select_time("animDtnS", selectHTMLArray, selectHTMLselected);
-    set_html_select_time("avgFrmTimeM", selectHTMLArray, selectHTMLselected);
-    
+    /* framerate select element */
+    var framerateArray = [12,24,25,30,48,50,60,72,90,100,120,144,300]
+    /* send params */
+    set_html_select_time("animDtnM", sexagesimalArray, 0);
+    set_html_select_time("animDtnS", sexagesimalArray, 0);
+    set_html_select_time("avgFrmTimeM", sexagesimalArray, 0);
+    set_html_select_time("frmRatePreset", framerateArray, 25);
+
+    var inputElmts = document.getElementsByClassName("watchUserInput");
+    for(var i = 0; i < inputElmts.length; i++){
+        inputElmts[i].addEventListener('input', ohYeah);
+    }
+
+    //    document.getElementsByClassName("inputTime").addEventListener('input', ohYeah);
+
+};
+
+function ohYeah(elt){
+    elt = elt.originalTarget
+    console.log(elt.id)
+    console.log(elt.value)
 }
 
 function set_html_select_time(selectHTMLElement, selectHTMLArray, selectHTMLselected){
+/*
+    Get a select element, and a table of values. You can send which element will be selected by default.
+*/
     selectHTMLElement = document.getElementById(selectHTMLElement);
     for (var i = 0; i < selectHTMLArray.length; i++) {
         var option = document.createElement("option");
@@ -22,5 +43,5 @@ function set_html_select_time(selectHTMLElement, selectHTMLArray, selectHTMLsele
           option.selected = "selected";
         };
         selectHTMLElement.appendChild(option);
-    }   
-}
+    };
+};
